6bc2ef505e4d88d4fd37e5f3dd34fddc6221b421
alssel2525 <alssel2525@naver.com>
2022-01-19 06:15:11 +0900

    언어 변경에 a 대신 Link 적용

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import Header from "./components/Header";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
 
 function App() {
 	return (
-		<>
+		<BrowserRouter>
 			<Header/>
-		</>
+			<Routes>
+				<Route exact path={"/"}/>
+			</Routes>
+		</BrowserRouter>
 	);
 }
 
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,6 +1,7 @@
 import styled from "styled-components"
 import logo from "../../images/logo_bg.png"
 import {useState} from "react";
+import {Link} from "react-router-dom";
 
 const Container = styled.div`
 	width: 100%;
@@ -8,6 +9,8 @@ const Container = styled.div`
 	margin: 0 auto;
 	position: relative;
 	z-index: 99;
+	padding: 0 calc((100vw - (1280px)) / 2);
+	box-sizing: border-box;
 
 	::before {
 		background-color: #f7f8fa;
@@ -35,11 +38,10 @@ const Container = styled.div`
 		nav {
 			position: relative;
 			float: right;
+			margin-top: 5rem;
 
 			#gnb {
-				padding-top: 5rem;
 				transition: all 0.3s;
-				margin: 0;
 				display: block;
 				list-style: none;
 
@@ -69,7 +71,6 @@ const Container = styled.div`
 						> li {
 							margin: 0 0 1rem;
 							list-style: none;
-							display: list-item;
 
 							> a {
 								position: relative;
@@ -102,12 +103,88 @@ const Container = styled.div`
 	}
 `
 
+const Utils = styled.div`
+	position: absolute;
+	right: 0;
+	padding-right: inherit;
+	box-sizing: border-box;
+	line-height: 1rem;
+	text-decoration: none;
+	font-weight: bold;
+
+	li {
+		list-style: none;
+	}
+
+	.site_map {
+		text-decoration: none;
+		display: inline-block;
+
+		a {
+			text-decoration: none;
+			color: #515151;
+		}
+	}
+
+	.lang {
+		::before {
+			content: "";
+			position: absolute;
+
+			background-color: #707070;
+			width: 1px;
+			height: 1rem;
+			opacity: 50%;
+		}
+
+		margin-left: 1rem;
+		display: inline-block;
+
+		ul {
+			padding: 0;
+		}
+
+		li {
+			display: inline-block;
+		}
+
+		a {
+			cursor: pointer;
+			text-decoration: none;
+			color: #BFBFBF;
+			margin-left: 1rem;
+		}
+
+		li.active a {
+			color: #B70023;
+		}
+	}
+`;
+
 const Header = () => {
 	const [isHover, setIsHover] = useState(false);
+	const [isKor, setIsKor] = useState(true);
 
 	return (
 		<Container onMouseEnter={() => setIsHover(true)} onMouseLeave={() => setIsHover(false)}
 			className={isHover ? "active" : ""}>
+			<Utils>
+				<ul>
+					<li className={"site_map"}>
+						<Link to={"/menu"}>SITE MAP</Link>
+					</li>
+					<li className={"lang"}>
+						<ul>
+							<li className={isKor ? "active" : ""}>
+								<Link to={"/menu"} onClick={() => setIsKor(true)}>KOR</Link>
+							</li>
+							<li className={isKor ? "" : "active"}>
+								<Link to={"/eng"} onClick={() => setIsKor(false)}>ENG</Link>
+							</li>
+						</ul>
+					</li>
+				</ul>
+			</Utils>
 			<div className={"inner"}>
 				<div className={"logo"}>
 					<a href={"/"}>
@@ -137,7 +214,8 @@ const Header = () => {
 								<li><a href={"/menu"}>공지사항</a></li>
 								<li><a href={"/menu"}>Q&A</a></li>
 								<li><a href={"/menu"}>자료실</a></li>
-							</ul></li>
+							</ul>
+						</li>
 						<li><a href={"/menu"}>홍보마당</a>
 							<ul className={"depth2"}>
 								<li><a href={"/menu"}>주요성과</a></li>
@@ -145,7 +223,8 @@ const Header = () => {
 								<li><a href={"/menu"}>포토갤러리</a></li>
 								<li><a href={"/menu"}>홍보자료</a></li>
 								<li><a href={"/menu"}>언론보도</a></li>
-							</ul></li>
+							</ul>
+						</li>
 						<li><a href={"/menu"}>특구사무국</a>
 							<ul className={"depth2"}>
 								<li><a href={"/menu"}>인사말</a></li>
@@ -153,7 +232,8 @@ const Header = () => {
 								<li><a href={"/menu"}>조직도(직원현황)</a></li>
 								<li><a href={"/menu"}>CI</a></li>
 								<li><a href={"/menu"}>오시는길</a></li>
-							</ul></li>
+							</ul>
+						</li>
 					</ul>
 				</nav>
 			</div>
