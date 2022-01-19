9f4a22e9e6d259d4b848aae9af75d946b2eec18d
alssel2525 <alssel2525@naver.com>
2022-01-20 08:24:51 +0900

    Utils.js 리팩토링

--- src/components/Header/Utils.js
+++ src/components/Header/Utils.js
@@ -7,6 +7,7 @@ const Container = styled.div`
 	box-sizing: border-box;
 	text-decoration: none;
 	font-weight: bold;
+
 	li {
 		list-style: none;
 	}
@@ -20,41 +21,38 @@ const Container = styled.div`
 			color: #515151;
 		}
 	}
-
-	.lang {
-		::before {
-			content: "";
-			position: absolute;
-
-			background-color: #707070;
-			width: 1px;
-			height: 1rem;
-			opacity: 50%;
-		}
-
-		margin-left: 1rem;
-		display: inline-block;
-
-		ul {
-			padding: 0;
-		}
-
-		li {
-			display: inline-block;
-		}
-
-		a {
-			cursor: pointer;
-			text-decoration: none;
-			color: #BFBFBF;
-			margin-left: 1rem;
-		}
-
-		li.active a {
-			color: #B70023;
-		}
-	}
 `;
+
+const Language = styled.ul`
+	margin-left: 1rem;
+	display: inline-block;
+	padding: 0;
+	line-height: 1rem;
+
+	::before {
+		content: "";
+		position: fixed;
+		background-color: #707070;
+		width: 1px;
+		height: 1rem;
+	}
+
+
+	li {
+		display: inline-block;
+	}
+
+	a {
+		cursor: pointer;
+		text-decoration: none;
+		color: #bdbdbd;
+		margin-left: 1rem;
+	}
+
+	li.active a {
+		color: #b71c1c;
+	}
+`
 
 const Utils = ({isKor, setIsKor}) => {
 	return (
@@ -63,16 +61,14 @@ const Utils = ({isKor, setIsKor}) => {
 				<li className={"site_map"}>
 					<Link to={"/menu"}>SITE MAP</Link>
 				</li>
-				<li className={"lang"}>
-					<ul>
-						<li className={isKor ? "active" : ""}>
-							<Link to={"/menu"} onClick={() => setIsKor(true)}>KOR</Link>
-						</li>
-						<li className={isKor ? "" : "active"}>
-							<Link to={"/eng"} onClick={() => setIsKor(false)}>ENG</Link>
-						</li>
-					</ul>
-				</li>
+				<Language>
+					<li className={isKor ? "active" : ""}>
+						<Link to={"/menu"} onClick={() => setIsKor(true)}>KOR</Link>
+					</li>
+					<li className={isKor ? "" : "active"}>
+						<Link to={"/eng"} onClick={() => setIsKor(false)}>ENG</Link>
+					</li>
+				</Language>
 			</ul>
 		</Container>
 	)
