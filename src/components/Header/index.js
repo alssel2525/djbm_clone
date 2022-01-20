352f430fd6f63ad35fba46ff4d7e4b1da7f9a97b
alssel2525 <alssel2525@naver.com>
2022-01-20 09:18:03 +0900

    GNB 분리

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -3,6 +3,7 @@ import logo from "../../images/logo_bg.png"
 import {useState} from "react";
 import {Link} from "react-router-dom";
 import Utils from "./Utils";
+import {menus} from "../../Data";
 
 const Container = styled.div`
 	width: 100vw;
@@ -104,6 +105,57 @@ const Container = styled.div`
 	}
 `
 
+const Gnb = styled.nav`
+	position: relative;
+	float: right;
+	margin-top: 5rem;
+	list-style: none;
+
+
+	#gnb {
+		transition: all 0.3s;
+		display: block;
+		list-style: none;
+
+		> li {
+			float: left;
+			margin-left: 6rem;
+			position: relative;
+
+			> a {
+				color: black;
+				text-transform: uppercase;
+				margin-bottom: 2rem;
+				font-weight: bold;
+				text-decoration: none;
+				font-size: 1.2rem;
+			}
+
+			.depth2 {
+				position: absolute;
+				top: 5rem;
+				left: 0;
+				width: 100%;
+				display: none;
+				white-space: nowrap;
+				padding: 0;
+
+				> li {
+					margin: 0 0 1rem;
+					list-style: none;
+
+					> a {
+						position: relative;
+						padding: 5px 0;
+						text-decoration: none;
+						color: #555555;
+					}
+				}
+			}
+		}
+	}
+`
+
 const Header = () => {
 	const [isHover, setIsHover] = useState(false);
 	const [isKor, setIsKor] = useState(true);
@@ -118,51 +170,20 @@ const Header = () => {
 						<img src={logo} alt={"biomedical"}/>
 					</Link>
 				</div>
-				<nav>
+				<Gnb>
 					<ul id={"gnb"}>
-						<li>
-							<Link to={"/menu"}>규제자유특구소개</Link>
-							<ul className={"depth2"}>
-								<li><a href={"/menu"}>규제자유특구란?</a></li>
-								<li><a href={"/menu"}>바이오메디컬 규제자유특구</a></li>
-								<li><a href={"/menu"}>특구사업자소개</a></li>
-							</ul>
-						</li>
-						<li>
-							<a href={"/menu"}>실증사업현황</a>
-							<ul className={"depth2"}>
-								<li><a href={"/menu"}>인체유래물은행</a></li>
-								<li><a href={"/menu"}>신의료기술 평가유예</a></li>
-								<li><a href={"/menu"}>병원체자원 공용연구시설</a></li>
-							</ul>
-						</li>
-						<li><a href={"/menu"}>알림마당</a>
-							<ul className={"depth2"}>
-								<li><a href={"/menu"}>공지사항</a></li>
-								<li><a href={"/menu"}>Q&A</a></li>
-								<li><a href={"/menu"}>자료실</a></li>
-							</ul>
-						</li>
-						<li><a href={"/menu"}>홍보마당</a>
-							<ul className={"depth2"}>
-								<li><a href={"/menu"}>주요성과</a></li>
-								<li><a href={"/menu"}>홍보동영상</a></li>
-								<li><a href={"/menu"}>포토갤러리</a></li>
-								<li><a href={"/menu"}>홍보자료</a></li>
-								<li><a href={"/menu"}>언론보도</a></li>
-							</ul>
-						</li>
-						<li><a href={"/menu"}>특구사무국</a>
-							<ul className={"depth2"}>
-								<li><a href={"/menu"}>인사말</a></li>
-								<li><a href={"/menu"}>추진경과/연혁</a></li>
-								<li><a href={"/menu"}>조직도(직원현황)</a></li>
-								<li><a href={"/menu"}>CI</a></li>
-								<li><a href={"/menu"}>오시는길</a></li>
-							</ul>
-						</li>
+						{menus.map((menu, index) => (
+							<li key={index}>
+								<Link to={"/"}>{menu.depth1}</Link>
+								<ul className={"depth2"}>
+									{menu.depth2.map((depth2item, index) => (
+										<li key={index}><Link to={"/"}/>{depth2item}</li>
+									))}
+								</ul>
+							</li>
+						))}
 					</ul>
-				</nav>
+				</Gnb>
 			</div>
 		</Container>
 	)
