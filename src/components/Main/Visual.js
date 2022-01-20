2a6267b7c3f095eaa83e4664f4fdecaf1298c942
alssel2525 <alssel2525@naver.com>
2022-01-20 13:25:12 +0900

    Arrow 추가

--- src/components/Main/Visual.js
+++ src/components/Main/Visual.js
@@ -1,8 +1,8 @@
 import styled from "styled-components";
-import {BiSearchAlt} from "react-icons/bi";
 import {useEffect, useRef, useState} from "react";
 import visual from "../../images/visual.jpg"
 import ButtonWithIcon from "./ButtonWithIcon";
+import {BsArrowLeftCircle, BsArrowRightCircle} from "react-icons/bs";
 
 const Container = styled.div`
 	height: 100vh;
@@ -15,7 +15,7 @@ const Container = styled.div`
 
 	.__1280 {
 		width: 1280px;
-		height: auto;
+		height: 100vh;
 		margin: 0 auto;
 		position: relative;
 		display: block;
@@ -26,13 +26,27 @@ const TextContainer = styled.div`
 	max-width: 100%;
 	height: auto;
 	position: relative;
-
 	margin-top: 20rem;
 	color: #777777;
 
 	strong {
 		color: #444444;
 		font-size: 3.5rem;
+	}
+`
+
+const Arrow = styled.div`
+	position: absolute;
+	top: 20rem;
+	right: 0;
+	color: white;
+
+	svg {
+		position: absolute;
+		width: 5rem;
+		height: 5rem;
+		right: 0;
+		top: 0;
 	}
 `
 
@@ -50,9 +64,11 @@ const Visual = () => {
 
 	return (
 		<Container>
-			<div style={{backgroundImage: `url(${visual})`, backgroundSize: "cover",
+			<div style={{
+				backgroundImage: `url(${visual})`, backgroundSize: "cover",
 				width: `calc(100vw - ${left}px)`, height: "100vh", left: left,
-				position: "absolute", overflow: "hidden"}}/>
+				position: "absolute", overflow: "hidden"
+			}}/>
 			<div className={"__1280"}>
 				<TextContainer>
 					<strong>{`DAEJEON Bio `}</strong>
@@ -65,6 +81,10 @@ const Visual = () => {
 					</span>
 				</TextContainer>
 				<ButtonWithIcon/>
+				<Arrow>
+					<BsArrowRightCircle/>
+					<BsArrowLeftCircle style={{top: "7rem"}}/>
+				</Arrow>
 			</div>
 		</Container>
 	)
