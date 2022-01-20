ff07fc03c84d8160f798a87a8e7a5d270993d174
alssel2525 <alssel2525@naver.com>
2022-01-21 03:53:06 +0900

    Carousel 구현

--- src/components/Main/Slider.js
+++ src/components/Main/Slider.js
@@ -28,7 +28,7 @@ const Slide = styled.div`
 		font-size: 1.8rem;
 		font-weight: bold;
 	}
-	
+
 	.yearMonth {
 		text-align: right;
 		font-size: 1rem;
@@ -39,9 +39,9 @@ const Slide = styled.div`
 		text-overflow: ellipsis;
 		white-space: nowrap;
 		overflow: hidden;
-		
+
 		margin-top: 0.3rem;
-		
+
 		font-size: 1.1rem;
 		font-weight: bold;
 	}
@@ -52,17 +52,34 @@ const Slide = styled.div`
 		display: -webkit-box;
 		-webkit-line-clamp: 2;
 		-webkit-box-orient: vertical;
-		
+
 		margin-top: 0.5rem;
-		
+
 		color: #878787;
 	}
 `
 
-const Slider = (carouselIndex) => {
+const Slider = ({carouselIndex}) => {
+	const array = Array.from(News);
+
+	const sortFn = (a, b) => {
+		return ((News.indexOf(a) + carouselIndex) % News.length) - ((News.indexOf(b) + carouselIndex) % News.length);
+	}
+
+	const pickColor = (color1, color2, weight) => {
+		const w1 = 1 - weight;
+		const w2 = weight;
+		const rgb = [Math.round(color1[0] * w1 + color2[0] * w2),
+			Math.round(color1[1] * w1 + color2[1] * w2),
+			Math.round(color1[2] * w1 + color2[2] * w2),];
+		console.log(rgb);
+		return rgb
+	}
+
 	return (<Container>
-		{News.map((news, index) => (
-			<Slide key={index}>
+		{array.sort(sortFn).map((news, index) => (
+			<Slide key={index}
+				style={{backgroundColor: `rgb(${pickColor([183, 28, 28], [48, 63, 159], News.indexOf(news) / News.length).join(",")})`}}>
 				<div className={"day"}>{news.date[1]}</div>
 				<div className={"yearMonth"}>{news.date[0]}</div>
 				<div className={"title"}>{news.title}</div>
