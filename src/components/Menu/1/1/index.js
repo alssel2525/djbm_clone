9edb07873a41450d2c1007a62d572c6df9dcefd3
alssel2525 <alssel2525@naver.com>
2022-02-07 20:51:22 +0900

    index Container를 Menu에 공통으로 쓰일 MenuContainer를 분리

--- src/components/Menu/1/1/index.js
+++ src/components/Menu/1/1/index.js
@@ -1,33 +1,21 @@
-import styled from "styled-components";
 import MenuContentTitle from "../../common/MenuContentTitle";
 import {useParams} from "react-router-dom";
 import {Menus} from "../../../../Data";
 import Content1 from "./Content1";
 import Content2 from "./Content2";
 import Content3 from "./Content3";
-
-const Container = styled.div`
-	width: 100vw;
-	max-width: 1280px;
-	margin: 0 auto;
-	padding-bottom: 3.5rem;
-	position: relative;
-	
-	> * {
-		margin-top: 3.5rem;
-	}
-`;
+import MenuContainer from "../../common/MenuContainer";
 
 const Menu11 = () => {
 	const params = useParams();
 	
 	return (
-		<Container>
+		<MenuContainer>
 			<MenuContentTitle>{Menus[parseInt(params.depth1) - 1].depth2[parseInt(params.depth2) - 1]}</MenuContentTitle>
 			<Content1/>
 			<Content2/>
 			<Content3/>
-		</Container>
+		</MenuContainer>
 	)
 };
 
---
+++ src/components/Menu/common/MenuContainer.js
@@ -0,0 +1,23 @@
+import styled from "styled-components";
+
+const Container = styled.div`
+	width: 100vw;
+	max-width: 1280px;
+	margin: 0 auto;
+	padding-bottom: 3.5rem;
+	position: relative;
+
+	> * {
+		margin-top: 3.5rem;
+	}
+`;
+
+const MenuContainer = (props) => {
+	return (
+		<Container>
+			{props.children}
+		</Container>
+	)
+};
+
+export default MenuContainer;
