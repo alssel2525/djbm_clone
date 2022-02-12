a823311db0f7d0490d809af8fb8b708a608aca41
alssel2525 <alssel2525@naver.com>
2022-02-13 02:58:21 +0900

    MenuContentTitle에서 param을 사용하도록 수정

--- src/components/Menu/1/2/index.js
+++ src/components/Menu/1/2/index.js
@@ -1,18 +1,14 @@
 import MenuContainer from "../../common/MenuContainer";
 import MenuContentTitle from "../../common/MenuContentTitle";
-import {useParams} from "react-router-dom";
-import {Menus} from "../../../../Data";
 import Content1 from "./Content1";
 import Content2 from "./Content2";
 import Content3 from "./Content3";
 import Content4 from "./Content4";
 
 const Menu12 = () => {
-	const params = useParams();
-	
 	return (
 		<MenuContainer>
-			<MenuContentTitle>{Menus[parseInt(params.depth1) - 1].depth2[parseInt(params.depth2) - 1]}</MenuContentTitle>
+			<MenuContentTitle/>
 			<Content1/>
 			<Content2/>
 			<Content3/>
---
+++ src/components/Menu/1/3/index.js
@@ -0,0 +1,12 @@
+import MenuContainer from "../../common/MenuContainer";
+import MenuContentTitle from "../../common/MenuContentTitle";
+
+const Menu13 = () => {
+	return (
+		<MenuContainer>
+			<MenuContentTitle/>
+		</MenuContainer>
+	)
+};
+
+export default Menu13;
