5ab6cd788af22500890850fdf5ef7f4f87b1460d
alssel2525 <alssel2525@naver.com>
2022-02-18 02:58:33 +0900

    표 css 작성

--- src/components/Menu/2/1/Tab2.js
+++ src/components/Menu/2/1/Tab2.js
@@ -48,14 +48,20 @@ const Table = styled.table`
 
 		> tr {
 			counter-increment: index;
+			line-height: 2rem;
 			
 			> td {
+				height: 100px;
+				padding: 40px;
 				background: ${Color.white};
 				border: 1px solid ${Color.lightgrey};
-				height: 100px;
 				position: relative;
+				box-sizing: border-box;
 				
+				// 운영절차
 				:nth-child(1) {
+					font-weight: 500;
+					
 					::before {
 						content: counter(index, decimal-leading-zero);
 						width: 2.5rem;
@@ -72,6 +78,76 @@ const Table = styled.table`
 						font-family: 'Sora', sans-serif;
 						font-weight: 600;
 					}
+					
+					// 2. 'IRB 심의'의 표
+					.review_grid {
+						margin-top: 1rem;
+						display: grid;
+						grid-gap: 5px;
+						font-weight: 400;
+						line-height: 1.5rem;
+						
+						div {
+							padding: 1rem;
+							display: flex;
+							flex-direction: column;
+							justify-content: center;
+							align-items: center;
+							border: 1px solid ${Color.lightgrey};
+							border-radius: 10px;
+							
+							:nth-child(1) {
+								grid-row: 1 / 3;
+								grid-column: 1 / 2;
+							}
+							:nth-child(2) {
+								grid-row: 1 / 2;
+								grid-column: 2 / 3;
+							}
+							:nth-child(3) {
+								grid-row: 2 / 3;
+								grid-column: 2 / 3;
+								background: ${Color.blueLighter}30;
+								color: ${Color.red};
+							}
+						}
+					}
+				}
+				
+				// 추진주체
+				:nth-child(2) {
+					div.arrow-container {
+						span {
+							position: relative;
+							
+							:not(:first-child) {
+								margin-left: 2rem;
+								
+								::before {
+									content: "play_arrow";
+									font-family: Material Icons, sans-serif;
+									width: 1.4rem;
+									height: 1.4rem;
+									line-height: 1.4rem;
+									font-size: 1rem;
+									position: absolute;
+									left: -1rem;
+									top: 50%;
+									transform: translate(-50%, -50%);
+									border-radius: 50%;
+									text-align: center;
+									vertical-align: middle;
+									background: ${Color.blue};
+									color: ${Color.white};
+								}
+							}
+						}
+					}
+				}
+				
+				// 주요내용
+				:nth-child(3) {
+					text-align: left;
 				}
 			}
 		}
@@ -97,14 +173,30 @@ const Tab2 = ({isActive}) => {
 						<td>- 수요 검체 조건 등에 대한 세부 논의</td>
 					</tr>
 					<tr>
-						<td>기업 맞춤형 컨설팅</td>
-						<td>인체유래물은행(대전TP)</td>
-						<td>- 수요 검체 조건 등에 대한 세부 논의</td>
-					</tr>
-					<tr>
-						<td>기업 맞춤형 컨설팅</td>
-						<td>인체유래물은행(대전TP)</td>
-						<td>- 수요 검체 조건 등에 대한 세부 논의</td>
+						<td>
+							<span>IRB 심의</span>
+							<div className={"review_grid"}>
+								<div>IRB 심의 전<br/>진행 사항</div>
+								<div>IRB* 협약**</div>
+								<div>보건복지부에 등록</div>
+							</div>
+						</td>
+						<td>
+							<span>공동운영기관IRB(충남대 IRB)</span>
+							<div className={"arrow-container"}>
+								<span>기업</span>
+								<span>충남대 IRB</span>
+							</div>
+							<div className={"arrow-container"}>
+								<span>기업</span>
+								<span>보건복지부</span>
+							</div>
+						</td>
+						<td>
+							- 연구계획 심의<br/>
+							- 공동운영기관IRB와 기업간 1:1 협약(서면)<br/>
+							- 협약 후 보건복지부에 협약 IRB 등록(약 한달 소요)
+						</td>
 					</tr>
 					<tr>
 						<td>기업 맞춤형 컨설팅</td>
